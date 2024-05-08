e7e9deb83af8e3ebf36ac966d4b3842c1ee9b7df
Sidhant Acharya <acharyasid1@gmail.com>
2024-05-08 16:01:26 +0545

    category

---
+++ gpt.py
@@ -0,0 +1,27 @@
+from openai import OpenAI
+
+client = OpenAI(api_key="sk-proj-J4ue6yaw1YyVgfqhayOIT3BlbkFJ9VyoB8dDMUsIN5rjxDRq")
+
+#while True:
+ask = input("Enter Link:")
+
+ #   if ask == "break":
+  #      print("Thank you.")
+   #     break
+    #else:
+response = client.chat.completions.create(
+            model="gpt-3.5-turbo",
+            messages=[
+                {
+                    "role": "user",
+                    "content": f"What is the category of website {ask}"
+                }
+            ],
+            temperature=1,
+            max_tokens=256,
+            top_p=1,
+            frequency_penalty=0,
+            presence_penalty=0
+        )
+print(response.choices[0].message.content)
+       
